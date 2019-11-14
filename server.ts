74bbd90e10756b39f5955147cb09c89af3cbdde5
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-11-14 16:06:55 +0700

    fix

--- server.ts
+++ server.ts
@@ -1,14 +1,43 @@
 import 'zone.js/dist/zone-node';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 // Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
-import {join} from 'path';
+import { join } from 'path';
 
 require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+
+const domino = require('domino');
+const fs = require('fs');
+const path = require('path');
+
+const template = fs.readFileSync(path.join(DIST_FOLDER, 'index.html')).toString();
+// for mock global window by domino
+const win = domino.createWindow(template);
+// mock
+global['window'] = win;
+// not implemented property and functions
+Object.defineProperty(win.document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
+
+// mock documnet
+global['document'] = win.document;
+// othres mock
+global['CSS'] = null;
+// global['XMLHttpRequest'] = require('xmlhttprequest').XMLHttpRequest;
+global['Prism'] = null;
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -16,11 +45,8 @@ enableProdMode();
 // Express server
 export const app = express();
 
-const PORT = process.env.PORT || 8080;
-const DIST_FOLDER = join(process.cwd(), 'dist/browser');
-
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
